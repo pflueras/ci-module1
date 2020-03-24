fdaf15f827c822d7728e3484240fabca460ddf5c
Petru Flueras <petru.flueras@gmail.com>
2020-03-24 17:50:04 +0200

    Added a simple Calculator

---
+++ src/main/java/org/examples/module1/Calculator.java
@@ -0,0 +1,14 @@
+package org.examples.module1;
+
+public class Calculator {
+    /**
+     * Simple add operation.
+     *
+     * @param a First operand
+     * @param b Second operand
+     * @return Sum of two operands
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
