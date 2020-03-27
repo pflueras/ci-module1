f32ffc782ab599037bbb46bc15e74b71de6f7ef7
Petru Flueras <petru.flueras@gmail.com>
2020-03-27 22:07:09 +0200

    Trigger downstream releases and builds

--- .travis/travis_downstream_deploy.sh
+++ .travis/travis_downstream_deploy.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
-echo $TRAVIS_TAG
-echo $TRAVIS_BRANCH
+echo 'Tag:' $TRAVIS_TAG
+echo 'Branch:' $TRAVIS_BRANCH
+
+git config --global user.email "petru.flueras@gmail.com"
+git config --global user.name "Petru Flueras"
+
+cd $TRAVIS_BUILD_DIR/..
+git clone --depth=50 --branch=master https://${GITHUB_TOKEN}@github.com/pflueras/ci-module2.git
+cd ci-module2
+
+# Update project version
+mvn versions:set -DnewVersion=$TRAVIS_TAG
+
+# Update version for all org.examples dependencies
+mvn versions:use-dep-version -Dincludes=org.examples -DdepVersion=$TRAVIS_TAG -DforceVersion=true
+
+git add pom.xml
+git commit -m "Version update: $TRAVIS_TAG"
+git push
