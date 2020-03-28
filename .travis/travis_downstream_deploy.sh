45995f59dcc164495a6a31bc3e5711609aa17ae5
Petru Flueras <petru.flueras@gmail.com>
2020-03-29 01:46:19 +0200

    Travis config

--- .travis/travis_downstream_deploy.sh
+++ .travis/travis_downstream_deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo 'Tag:' $TRAVIS_TAG
-echo 'Branch:' $TRAVIS_BRANCH
 
 git config --global user.email "petru.flueras@gmail.com"
 git config --global user.name "Petru Flueras"
@@ -10,15 +9,27 @@ cd $TRAVIS_BUILD_DIR/..
 git clone --depth=50 --branch=master https://${GITHUB_TOKEN}@github.com/pflueras/ci-module2.git
 cd ci-module2
 
-# Update project version
+# Update project version and org.examples dependencies
 mvn versions:set -DnewVersion=$TRAVIS_TAG
-
-# Update version for all org.examples dependencies
 mvn versions:use-dep-version -Dincludes=org.examples -DdepVersion=$TRAVIS_TAG -DforceVersion=true
 
 git add pom.xml
-git commit -m "Version update: $TRAVIS_TAG"
-git tag "$TRAVIS_TAG" -m "Travis CI version update $TRAVIS_TAG"
+git commit -m "Release version $TRAVIS_TAG"
+git tag "$TRAVIS_TAG" -m "Release version $TRAVIS_TAG"
+
+# New version
+MAJOR=$(echo $TRAVIS_TAG | cut -f 1 -d '.')
+MINOR=$(echo $TRAVIS_TAG | cut -f 2 -d '.')
+PATCH=$(echo $TRAVIS_TAG | cut -f 3 -d '.')
+NEW_VERSION=$MAJOR.$MINOR.$(($patch + 1))-SNAPSHOT
+echo 'New version:' $NEW_VERSION
+
+# Prepare for next development version
+mvn versions:set -DnewVersion=$NEW_VERSION
+mvn versions:use-dep-version -Dincludes=org.examples -DdepVersion=$NEW_VERSION -DforceVersion=true
+git add pom.xml
+git commit -m "Next development version $NEW_VERSION"
+
 git push --follow-tags
 
 # Clean up
